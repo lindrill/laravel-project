31d4c9634411fb911f4a167293262088e0c0bd8b
lindrill <lindrillagbong@gmail.com>
2020-07-25 00:42:59 +0800

    show available stocks per product in the dashboard, edit stocks view and update stocks

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Product;
 use App\Delivery;
 use DB;
+use Validator;
 
 class HomeController extends Controller
 {
@@ -27,27 +28,75 @@ class HomeController extends Controller
     public function index()
     {
         $data = array();
-        // $products = DB::table('products')
-        //     ->leftJoin('deliveries', 'products.id', '=', 'deliveries.product_id')
-        //     ->select('deliveries.*', 'products.*')
-        //     ->get();
         $deliveries = Delivery::all();
-        // return view('delivery.index', compact('deliveries'));
-
         $products = Product::all();
 
-        // foreach ($deliveries as $delivery) {
-        //     $data['id'] = $product->id;
-        //     $data['name'] = $product->name;
-
-            foreach ($deliveries as $delivery) {
-                // if($delivery->product_id == $product->id) {
-                    $data['quantity'] = $delivery->quantity;
-                // }
+        $del = [];
+        $p_ids = [];
+        $qty;
+        foreach ($deliveries as $delivery) {
+            foreach ($products as $product) {
+                if($delivery->product_id == $product->id) {
+                    if(empty($data)) { // first iteration
+                        $qty = $delivery->quantity;
+                        $del['quantity'] = $qty;
+                        $del['product_id'] = $delivery->product_id;
+                        $del['product_name'] = $product->name;
+                        array_push($p_ids, $delivery->product_id); 
+                        array_push($data, $del);
+                    } else {
+                        if(in_array($delivery->product_id, $p_ids)) {
+                            foreach ($data as $key => $value) {
+                                if($delivery->product_id == $value['product_id']) {
+                                    $data[$key]['quantity'] += $delivery->quantity;
+                                }
+                            }
+                        } else {
+                            $qty = $delivery->quantity;
+                            $del['quantity'] = $qty;
+                            $del['product_id'] = $delivery->product_id;
+                            $del['product_name'] = $product->name;
+                            array_push($p_ids, $delivery->product_id); 
+                            array_push($data, $del);
+                        }
+                    }
+                }
             }
-        // }
-        dd($data);
+        }
 
-        return view('home', compact('products', 'deliveries'));
+        return view('home', compact('data'));
+    }
+
+    public function edit($product_id)
+    {
+        $delivery = DB::table('deliveries')
+            ->join('products', 'products.id', '=', 'deliveries.product_id')
+            ->select('deliveries.*', 'products.name')
+            ->where('product_id', $product_id)
+            ->latest()
+            ->first();
+
+        return view('delivery.edit-stocks', compact('delivery'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $info_validator = Validator::make($request->all(), [
+            'quantity' => 'required|integer',
+        ]);
+
+        $delivery = Delivery::find($id);
+        $delivery->quantity = $request->quantity;
+        $delivery->save();
+        if($delivery->save()) {
+            return back()->with('message', 'Delivery updated successfully!');
+        }
     }
 }
