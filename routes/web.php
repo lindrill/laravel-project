1c97be5f3dba7d5db698c2d4225d2cdef8b491f4
lindrill <lindrillagbong@gmail.com>
2020-07-25 00:44:12 +0800

    added routes for stocks edit and update

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/dashboard', 'HomeController@index')->name('dashboard')->middleware('admin');
+Route::get('/stocks/{id}/edit', 'HomeController@edit')->middleware('admin');
+Route::patch('/stocks/{id}', 'HomeController@update')->middleware('admin');
 Route::resource('/users', 'UserController')->middleware('admin');
 Route::resource('/products', 'ProductController')->middleware('admin');
 Route::resource('/deliveries', 'DeliveryController')->middleware('admin');
