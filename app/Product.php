0b382e950cfd1fe1f8abacacaf7e8bf7520e62ca
lindrill <lindrillagbong@gmail.com>
2020-07-25 20:24:21 +0800

    product softdeletes

--- app/Product.php
+++ app/Product.php
@@ -3,14 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+	use SoftDeletes;
+
     protected $primaryKey = 'id';
 
     protected $fillable = [
         'name', 'description', 'photo', 'unit_price'
     ];
+
+    protected $dates = ['deleted_at'];
 
     public function delivery() {
         return $this->hasMany('App\Delivery');
