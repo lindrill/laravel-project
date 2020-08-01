7d789f7c418edf666008b6405f1455b2482a0a00
lindrill <lindrillagbong@gmail.com>
2020-08-01 13:53:59 +0800

    created Cart model

---
+++ app/Cart.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Cart extends Model
+{
+    use SoftDeletes;
+
+    protected $primaryKey = 'id';
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'product_id', 'user_id', 'quantity', 'amount'
+    ];
+
+    public function product() {
+        return $this->belongsTo('App\Product');
+    }
+
+    public function user() {
+        return $this->hasMany('App\Users');
+    }
+}
