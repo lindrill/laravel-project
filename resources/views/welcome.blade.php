58c323845babd28fddb956ebbe83f7d29db1d811
lindrill <lindrillagbong@gmail.com>
2020-08-01 13:57:05 +0800

    display only items with != 0 available stocks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,17 +20,28 @@
             </div>
         </div>
     </div>
+    @if (session('message'))
+        <div class="alert alert-success" role="alert">
+            {{ session('message') }}
+        </div>
+    @endif
     <div id="products_section" class="d-flex flex-wrap justify-content-center">
-        @foreach($products as $product)
+        @foreach($data as $d)
         <div class="p-3">
             <div class="card">
-                <a href="{{ url('stock', [$product->id]) }}">
-                    <img src="{{ asset('/images/products/'.$product->photo) }}" alt="product image" width="250">
+                
+                <a href="{{ url('stock', [$d['product_id']]) }}">
+                    <img src="{{ asset('/images/products/'.$d['img']) }}" alt="product image" width="250">
                 </a>
                 <div class="text-center mt-2">
-                    <h6><a href="{{ url('stock', [$product->id]) }}">{{ $product->name }}</a></h6>
-                    <h5>{{ $product->unit_price }}</h5>
-                    <button type="submit" class="btn btn-warning btn-block">Add to cart</button>
+                    <h6><a href="{{ url('stock', [$d['product_id']]) }}">{{ $d['product_name'] }}</a></h6>
+                    <h5>{{ $d['unit_price'] }}</h5>
+                    <form method="POST" action="{{ url('cart') }}">
+                        @csrf
+                        @method('POST')
+                        <input name="product_id" type="hidden" value="{{ $d['product_id'] }}">
+                        <button type="submit" class="btn btn-warning btn-block">Add to cart</button>
+                    </form>
                 </div>
             </div>
         </div>
@@ -47,6 +58,40 @@ $(document).ready(function(){
     var img_url = "{{ asset('/images/products/') }}";
 
     function load_data(search_product_query = '') {
+        var search_val = $("#search_text").val();
+        var _token = $("input[name=_token]").val();
+        $.ajax({
+            url: "/search_product",
+            method: "GET",
+            data: {search: search_val, _token:_token},
+            dataType: "json",
+            success: function(data) {
+                var output = '';
+                if(data.length > 0) {
+                    for(var count = 0; count < data.length; count++) {
+                        output += '<div class="p-3">';
+                        output += '<div class="card">';
+                        output += '<a href="stock/'+data[count].product_id+'">';
+                        output += '<a href="stock/'+data[count].product_id+'">';
+                        output += '<img src="'+img_url+'/'+data[count].photo+'" alt="product image" width="250">';
+                        output += '</a>';
+                        output += '<div class="text-center mt-2">';
+                        output += '<h6><a href="stock/"'+data[count].product_id+'>'+data[count].name+'</a></h6>';
+                        output += '<h5>'+data[count].unit_price+'</h5>';
+                        output += '<button class="btn btn-warning btn-block">Add to cart</button>';
+                        output += '</div>';
+                        output += '</div>';
+                        output += '</div>';
+                    }
+                } else {
+                    output += "No data found";
+                }
+                $('#products_section').html(output);
+            }
+        });
+    }
+
+    function add_to_cart(search_product_query = '') {
         var search_val = $("#search_text").val();
         var _token = $("input[name=_token]").val();
         $.ajax({
