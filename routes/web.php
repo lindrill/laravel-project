cd959ae2cd1b6cfd71b430dcd5e7b65b44f16ebd
lindrill <lindrillagbong@gmail.com>
2020-08-01 13:55:55 +0800

    added new resource and route for CartController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Product;
+use App\Delivery;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +16,17 @@ use App\Product;
 */
 
 Route::get('/', function () {
+	$deliveries = Delivery::all();
 	$products = Product::all();
-    return view('welcome', compact('products'));
+	$data = app('App\Http\Controllers\HomeController')->get_stocks($deliveries, $products);
+    return view('welcome', compact('data'));
 });
 
 Auth::routes();
 
 Route::get('/dashboard', 'HomeController@index')->name('dashboard')->middleware('admin');
 Route::get('/stocks/{id}/edit', 'HomeController@edit')->middleware('admin');
-Route::get('/stock/{id}', 'HomeController@show');
+Route::get('/stock/{id}', 'HomeController@show')->middleware('admin');
 Route::patch('/stocks/{id}', 'HomeController@update')->middleware('admin');
 Route::resource('/users', 'UserController')->middleware('admin');
 Route::resource('/products', 'ProductController')->middleware('admin');
@@ -31,3 +34,5 @@ Route::resource('/deliveries', 'DeliveryController')->middleware('admin');
 Route::get('/change-password/{id}/edit', 'UserController@change_password')->middleware('admin');
 Route::patch('/change-password/{id}/', 'UserController@update_password')->middleware('admin');
 Route::get('/search_product', 'ProductController@search_product');
+Route::resource('/cart', 'CartController');
+Route::put('/update_cart', 'CartController@update_cart');
